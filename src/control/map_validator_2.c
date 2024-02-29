b9bdadf500b23942aa1cd8ad95804036fd3e9f67
Jesus Munoz Bermudez <jesmunoz@c3r1s3.42malaga.com>
2024-02-29 16:36:18 +0100

    test_map_should_not_have_closed_walls() function finished

--- src/control/map_validator_2.c
+++ src/control/map_validator_2.c
@@ -6,7 +6,7 @@
 /*   By: jesmunoz <jesmunoz@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 11:41:08 by jesmunoz          #+#    #+#             */
-/*   Updated: 2024/02/29 14:17:59 by jesmunoz         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:34:05 by jesmunoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,12 @@ int	is_map_closed(char **map)
 	map_coordinates = get_map_dimesions(map);
 	if (!map_coordinates)
 		return (0);
-	printf("\nmap_coordinates[0] = %d\n", map_coordinates[0]);
-	printf("map_coordinates[1] = %d\n", map_coordinates[1]);
-	printf("map[0] = %s\n", map[0]);
-	printf("map[map_coordinates[1] - 1] = %s\n", map[map_coordinates[1] - 1]);
-	if (ft_strchr(map[0], '1') != 0 || ft_strchr(map[map_coordinates[1] - 1], '1') != 0)
-		return (0);
+	while (map[0][x] != '\0' && map[map_coordinates[1] - 1][x] != '\0')
+	{
+		if (map[0][x] != '1' || map[map_coordinates[1] - 1][x] != '1')
+			return (0);
+		x++;
+	}
 	printf("\n");
 	while (x < map_coordinates[1])
 	{
